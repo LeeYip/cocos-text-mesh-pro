aed03bc18d84229cf06b8b1bec06dbcb95f2dbfd
YipLee <2274086727@qq.com>
2022-12-04 22:06:42 +0800

    change: 插件描述和版本号

--- extensions/textmeshpro-tool/i18n/en.js
+++ extensions/textmeshpro-tool/i18n/en.js
@@ -1,1 +1,1 @@
-"use strict";module.exports={open_panel:"Default Panel",send_to_panel:"Send message to Default Panel",description:"Extension with a panel based on Vue3.x"};
+"use strict"; module.exports = { open_panel: "Default Panel", send_to_panel: "Send message to Default Panel", description: "A font generator tool for CocosTextMeshPro" };
